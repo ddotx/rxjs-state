62d0f825dfc065212106edeb3d285d2153668be8
Michael Hladky <michael@hladky.at>
2020-03-13 03:02:50 +0100

    feat(refactor to separate files)

--- libs/ngx-state/src/index.ts
+++ libs/ngx-state/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/index';
-
-
+export {RxState} from './lib/rx-state.service';
+export {RxGlobalState} from './lib/rx-global-state.service';
--- libs/ngx-state/src/lib/rx-global-state.service.ts
+++ libs/ngx-state/src/lib/rx-global-state.service.ts
@@ -1,9 +1,9 @@
-import {Injectable, OnDestroy} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {State} from '@rx-state/rxjs-state';
 
-@Injectable()
-export class RxState<T> extends State<T> implements OnDestroy {
+@Injectable({providedIn: 'root'})
+export class RxGlobalState<T> extends State<T> {
   subscription = new Subscription();
 
   constructor() {
@@ -11,7 +11,4 @@ export class RxState<T> extends State<T> implements OnDestroy {
     this.subscription.add(this.subscribe());
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
