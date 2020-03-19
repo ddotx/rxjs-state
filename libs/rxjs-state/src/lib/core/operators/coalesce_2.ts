898fe8625dfa7cf39507d4e60f4c35a2a2a5c3b5
Michael Hladky <michael@hladky.at>
2020-03-19 19:57:25 +0100

    feat(coalesce): backup old status

---
+++ libs/rxjs-state/src/lib/core/operators/coalesce_2.ts
@@ -0,0 +1,55 @@
+import {Observable, concat, EMPTY, OperatorFunction, MonoTypeOperatorFunction, defer} from 'rxjs';
+import {publish, first,tap,filter, concat as concatWith, share,shareReplay, throttle, takeUntil,last, take,withLatestFrom, map, materialize, defaultIfEmpty, switchMap} from 'rxjs/operators';
+import {CoalesceConfig, getCoalesceWorkConfig} from '../utils';
+
+
+export function coalesce<T>(cfg?: CoalesceConfig): MonoTypeOperatorFunction<T> {
+  return (o: Observable<T>): Observable<T> => {
+    let isFirstEmitted = false;
+    const preparedCfg: CoalesceConfig = getCoalesceWorkConfig(cfg);
+    const inputComplete$ = o.pipe(materialize());
+    const animationFrameFirstEmission$ = animationFrames().pipe(
+      take(1)
+    );
+    const first$  = o.pipe(first(), defaultIfEmpty(), tap({complete: () => console.info('first$ Complete')}));
+    const last$ = o.pipe(takeUntil(animationFrameFirstEmission$), last(), tap({complete: () => console.info('last$ Complete')}));
+    return o.pipe(
+      throttle(val => animationFrameFirstEmission$, preparedCfg),
+    );
+  };
+}
+
+// @TODO delete when decide for rxjs target version
+export interface TimestampProvider {
+  now(): number;
+}
+
+
+export function animationFrames(timestampProvider: TimestampProvider = Date) {
+  return timestampProvider === Date ? DEFAULT_ANIMATION_FRAMES : animationFramesFactory(timestampProvider);
+}
+
+/**
+ * Does the work of creating the observable for `animationFrames`.
+ * @param timestampProvider The timestamp provider to use to create the observable
+ */
+function animationFramesFactory(timestampProvider: TimestampProvider) {
+  return new Observable<number>(subscriber => {
+    let id: number;
+    const start = timestampProvider.now();
+    const run = () => {
+      subscriber.next(timestampProvider.now() - start);
+      if (!subscriber.closed) {
+        id = requestAnimationFrame(run);
+      }
+    };
+    id = requestAnimationFrame(run);
+    return () => cancelAnimationFrame(id);
+  });
+}
+
+/**
+ * In the common case, where `Date` is passed to `animationFrames` as the default,
+ * we use this shared observable to reduce overhead.
+ */
+const DEFAULT_ANIMATION_FRAMES = animationFramesFactory(Date);
